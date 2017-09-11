6fb032ba61a1e9aecc697d9bdc65fe6daa0013e4
jun <137322828@qq.com>
2017-09-11 11:11:53 +0800

    学科分类列表实现

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,3 +1,7 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+//渲染学科列表
+$.get('/v6/category', function(data) {
+    $('.table-bordered').append(template('category-list-tpl', data.result));
+});
