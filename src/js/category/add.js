d5d7dc0665fb6ed1b4b770f6c99551c647f20192
jun <137322828@qq.com>
2017-09-11 20:33:56 +0800

    添加学科完成，修改页面导航文字，登陆页头像完成

--- src/js/category/add.js
+++ src/js/category/add.js
@@ -1,3 +1,15 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+//动态渲染顶级学科
+$.get('/v6/category/top', function(data) {
+    if (data.code == 200) {
+        $('#category-top-select').html(template('select-tpl', data.result));
+    }
+});
+//表单提交
+$('#category-add-form').ajaxForm(function(data) {
+    if (data.code == 200) {
+        alert('恭喜你创建了一个学科');
+    }
+});
--- src/js/user/login.js
+++ src/js/user/login.js
@@ -12,3 +12,7 @@ $("#login-form").ajaxForm({
         alert('登录失败');
     }
 });
+//回显历史登录用户的头像，没有就展示一个默认头像
+var userinfo = JSON.parse(localStorage.getItem('userinfo')) || {};
+var tc_avatar = userinfo.tc_avatar || '/public/img/default.png';
+$('.avatar img').attr('src', tc_avatar);
