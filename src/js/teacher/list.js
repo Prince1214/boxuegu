5b199d59a105e408815a639d331baceac8ed9e72
jun <137322828@qq.com>
2017-09-12 14:48:48 +0800

    讲师列表的启用和注销

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -3,5 +3,18 @@ require('../common/header.js');
 require('../common/aside.js');
 //列表数据显示
 $.get('/v6/teacher', function(data) {
-    data.code == 200 && $('#teacher-list-table').append(template('teacher-list-tpl', data.result))
-})
+    data.code == 200 && $('#teacher-list-table').append(template('teacher-list-tpl', data.result));
+});
+//讲师启用与注销
+$(document).on('click', '.btn-teacher', function() {
+    var $this = $(this);
+    var data = {
+        tc_id: $(this).attr('data-id'),
+        tc_status: $(this).attr('data-status')
+    };
+    $.post('/v6/teacher/handle', data, function(data) {
+        var newStatus = data.result.tc_status;
+        $this.text(newStatus == 0 ? '注销' : '启用');
+        $this.attr('data-status', newStatus);
+    });
+});
