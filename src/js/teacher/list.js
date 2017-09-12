5454d893a1227752a7dbecbba8cb3f54289aefae
jun <137322828@qq.com>
2017-09-12 10:45:38 +0800

    讲师列表渲染

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,3 +1,7 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+//列表数据显示
+$.get('/v6/teacher', function(data) {
+    data.code == 200 && $('#teacher-list-table').append(template('teacher-list-tpl', data.result))
+})
