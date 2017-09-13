86b5c4102cb07788c51854904d7cc6d70feb4507
jun <137322828@qq.com>
2017-09-13 20:23:12 +0800

    课程编辑第三步数据回显

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -1,3 +1,12 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util.js');
+var cs_id = util.getSearch('cs_id');
+//数据回显
+$.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        data.result.editIndex = 3;
+        $('#course-edit3').append(template('course-edit3-tpl', data.result));
+    }
+});
