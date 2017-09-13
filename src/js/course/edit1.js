22c229559830d2d59a66edebd067a85aee2dae80
jun <137322828@qq.com>
2017-09-13 19:08:54 +0800

    课程编辑第二步数据显示

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -1,7 +1,7 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
-var util = require('../common/util');
+var util = require('../common/util.js');
 
 var cs_id = util.getSearch('cs_id');
 //数据回显
--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -1,3 +1,11 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util.js');
+var cs_id = util.getSearch('cs_id');
+//数据回显
+$.get('/v6/course/picture', { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        $('#course-edit2').append(template('course-edit2-tpl', data.result));
+    }
+});
