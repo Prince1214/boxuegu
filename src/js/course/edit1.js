5c8ff3fceb97db196e3f84f78ddbdc67c351909f
jun <137322828@qq.com>
2017-09-13 15:14:11 +0800

    课程编辑数据回显

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -1,3 +1,12 @@
 //首页导入退出功能
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util');
+
+var cs_id = util.getSearch('cs_id');
+//数据回显
+$.get('/v6/course/basic', { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        $('#course-edit1').append(template('course-edit1-tpl', data.result));
+    }
+});
