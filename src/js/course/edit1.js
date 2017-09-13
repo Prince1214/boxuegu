aca484f079a1e9655da103b2c8fe8b9502d95cff
jun <137322828@qq.com>
2017-09-13 15:49:49 +0800

    课程编辑第一数据提交

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -10,3 +10,31 @@ $.get('/v6/course/basic', { cs_id: cs_id }, function(data) {
         $('#course-edit1').append(template('course-edit1-tpl', data.result));
     }
 });
+//学科二级联动
+$(document).on('change', '#category-top-select', function() {
+    //select的value就是用户所选的顶级学科cg_id
+    var topCgid = $(this).val();
+    //利用顶级学科cg_id获取对应子级学科列表
+    $.get('/v6/category/child', { cg_id: topCgid }, function(data) {
+        var html = "";
+        var chidList = data.result;
+        //根据子级列表动态生成对应的option
+        if (data.code == 200) {
+            for (var i = 0, len = chidList.length; i < len; i++) {
+                html += '<option value="' + chidList[i].cg_id + '">' + chidList[i].cg_name + '</option>'
+            }
+        }
+        //使用新的option进行替换
+        $('#category-child-select').html(html);
+    });
+});
+//表单提交
+$('#course-edit1-form').ajaxForm({
+    delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            alert('修改成功');
+            location.href = '/dist/html/course/edit2.html?cs_id=' + cs_id;
+        }
+    }
+});
